9d04feb961b5bbf5da7548e388dfcdabd50fb444
upamachy <upamachy360@gmail.com>
2025-08-26 23:54:33 +0600

    model created

--- MediatorDemo/BlazorUI/obj/Debug/net7.0/BlazorUI.AssemblyInfo.cs
+++ MediatorDemo/BlazorUI/obj/Debug/net7.0/BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb506769aeeb8ab3447b21566820a4b12dcdc58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c67b924f886458a0796439e32b69bde60bac46")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MediatorDemo/DemoApi/obj/Debug/net7.0/DemoApi.AssemblyInfo.cs
+++ MediatorDemo/DemoApi/obj/Debug/net7.0/DemoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb506769aeeb8ab3447b21566820a4b12dcdc58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c67b924f886458a0796439e32b69bde60bac46")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
